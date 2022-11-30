16e98049ead63c2dccc9e819f36c11aee7f74a88
Igor <venegas.igor@gmail.com>
2022-11-29 23:37:58 -0300

    se creo un seed que arma el select con las distintos tipos de denuncias

---
+++ script.js
@@ -0,0 +1,111 @@
+data = [
+    {nombre:'Servicio Agrícola Ganadero (SAG)' ,web: 'https://www.sag.gob.cl/ambitos-de-accion/atencion-de-denuncias',num: '(2) 23451100',correo: 'oficina.informaciones@sag.gob.cl', casos: [
+        "Caza y captura de fauna silvestre no hidrobiológica",
+        "Destrucción de nidos y madrigueras, recolección de huevos y crías de fauna nativa",
+        "Muerte masiva y/o cuadros clínicos masivos de enfermedades en animales",
+        "Control y fiscalización de uso de pesticidas y fertilizantes",
+        "Quema de residuos agrícolas o forestales"
+    ]},
+    {nombre:'Servicio nacional de Pesca y Acuicultura (SERNAPESCA)' ,web: 'http://www.sernapesca.cl/sistema-integral-de-informacion-y-atencion-ciudadana-siac',num: '800 320 032',correo: 'informaciones@sernapesca.cl', casos: [
+        "Afectación de fauna y recursos hidrobiológicos",
+        "Pesca recreativa en aguas bajo protección oficial",
+        "Pesca ilegal"
+    ]},
+    {nombre:'Dirección General de Aguas (DGA)' ,web: 'https://dga.mop.gob.cl/productosyservicios/realizarDenuncia/Paginas/denuncia.aspx',num: '',correo: 'dga.denunciavalpo@mop.gov.cl', casos: [
+        "Extracción de áridos no autorizada",
+        "Obras no autorizadas en cauce",
+        "Caudal ecológico mínimo",
+        "Control de extracciones de aguas superficiales y subterráneas",
+        "Incumplimiento del ejercicio del derecho de aprovechamiento de aguas",
+        "Captura y caza de fauna nativa",
+        "Destrucción de nidos y madrigueras",
+        "Contaminación de cuerpos de agua",
+        "Drenaje de humedales",
+        "Catastro de sistemas de drenajes"
+    ]},
+    {nombre:'Corporación Nacional Forestal (CONAF)' ,web: 'https://oficinavirtual.conaf.cl/denunciaTerceros/index.php?x=1',num: '',correo: '', casos: [
+        "Incendios forestales",
+        "Tala o corta de vegetación y árboles nativos",
+        "Actividades que afecten humedales en unidades del SNASPE",
+        "Corta de bosques nativos aledaños a humedales Ramsar",
+        "Afectación de humedales en zonas de protección de exclusión e intervención en zonas de protección de manejo limitado",
+        "Quema de residuos agrícolas o forestales",
+        "Abandono de mascotas en Parques Nacionales"
+    ]},
+    {nombre:'SEREMI de Salud' ,web: 'https://oirs.minsal.cl/',num: '',correo: '', casos: [
+        "Descarga de aguas servidas y residuos industriales",
+        "Autorización de camiones limpiafosas",
+        "Aplicación de pesticidas",
+        "Presencia de basura domiciliaria, escombros y otros dentro de patio de hogares",
+        "Olores/residuos de locales comerciales o de comida",
+        "Microbasurales con presencia de roedores u otros",
+        "Talleres de vehículos/muebles que emanan malos olores/residuos",
+        "Humos visibles de estufas y cocinas domiciliarias a leña",
+        "Polvo en suspensión por construcción de viviendas/edificios que afecten la salud de la comunidad de proyectos que no cuentan con RCA",
+        "Quema de basura en lugares urbanos"
+    ]},
+    {nombre:'Consejo de Monumentos Nacionales' ,web: 'https://www.monumentos.gob.cl/tramites/formulario-denuncia-monumento-nacional',num: '',correo: 'tramites_CMN@monumentos.gob.cl', casos: [
+        "Actividades que afectan a humedales que son parte de Santuarios de la Naturaleza"        
+    ]},
+    {nombre:'Ministerio de Bienes Nacionales' ,web: 'https://www.bienesnacionales.cl/?page_id=40502',num: '',correo: '', casos: [
+        "Actividades que afecten humedales en bienes nacionales de uso público, terrenos fiscales",
+        "Extracción de áridos no autorizados"
+    ]},
+    {nombre:'Superintendencia de Servicios Sanitarios (SISS)' ,web: 'https://www.siss.gob.cl/586/w3-propertyvalue-6438.html',num: '',correo: '', casos: [
+        "Descargas de residuos líquidos de empresas de servicios sanitarios",
+        "Descarga a alcantarillados de empresas concesionadas",
+        "Descargas de aliviaderos de tormentas",
+        "Mal olor de alcantarillado, aguas servidas y agua potable"
+    ]},
+    {nombre:'Superintendencia del Medio Ambiente (SMA) ' ,web: 'https://denuncia.sma.gob.cl/',num: '',correo: '', casos: [
+        "Descarga de residuos líquidos y/o contaminación de cuerpos de aguaDrenaje o relleno",
+        "Descarga de residuos líquidos industriales (RILes)",
+        "Extracción de caudales o de áridos",
+        "Alteración de vegetación azonal hídrica y ripariana",
+        "Actividades en áreas bajo protección oficial",
+        "Deterioro, menoscabo, transformación o invasión de la flora y fauna",
+        "Aplicación masiva de productos químicos",
+        "Actividades que afectan humedales urbanos declarados",
+        "Ruido molesto generado por actividades productivas y comerciales",
+        "Ruido molesto generado por instalaciones destinadas a la reacción,deporte, cultura, etc.",
+        "Ruido molesto generado por servicios de salud, educación , religioso,comunitario, etc.",
+        "Ruido molesto generado por faenas constructivas y elementos de infraestructura",
+        "Vibraciones u ondas electromagnéticas",
+        "Comercio de leña o de artefactos a leña no autorizados"
+    ]},
+    {nombre:'Carabineros' ,web: 'https://comisariavirtual.cl/',num: '352 535073',correo: '', casos: [
+        "Captura y caza de fauna nativa",
+        "Destrucción de nidos y madrigueras",
+        "Quemas no autorizadas",
+        "Abandono de animales como mascotas",
+        "Contaminación acústica por ruido de fiestas de vecinos",
+        "Ruidos de vehículos/humos visibles desde tubos de escape",
+        "Abandono o maltrato de mascotas en la vía pública"
+    ]},
+    {nombre:'Municipalidad de Cartagena' ,web: '',num: '352 200700',correo: '', casos: [
+        "Ruidos y/o malos olores que dejan las ferias libres",
+        "Olores de mascotas/tenencia irresponsable de animales domésticos",
+        "Presencia de basura domiciliaria, escombros y otros dentro de patio de hogares",
+        "Formación de microbasurales",
+        "Ruidos de vehículos/humos visibles desde tubos de escape",
+        "Polvo en suspensión por construcción de viviendas/edificios de proyectos que no cuentan con RCA",
+        "Utilización de bolsas plásticas en el comercio",
+    ]},
+    {nombre:'DIRECTEMAR' ,web: 'https://www.directemar.cl/directemar/servicios-online/procedimiento-de-tramitacion-de-quejas-online-para-tripulantes',num: '',correo: '', casos: [
+        "Vehículos motorizados en playas"
+    ]},
+    {nombre:'PDI' ,web: 'https://www.denunciaseguro.cl/',num: '',correo: '', casos: [
+        "Abandono o maltrato de mascotas en la via publica"
+    ]},
+]
+
+const sel = document.getElementById("denuncias");
+
+data.forEach((element) => {
+    element.casos.forEach((denuncia) =>{
+        opt = document.createElement("option");
+        opt.value = element.nombre;
+        opt.text = denuncia;
+        sel.add(opt, null);
+    });
+});
